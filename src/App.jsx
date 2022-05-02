f6fcb09c5b79a9a8af468996be8d71c50f9e4793
Brandon Franks <bgfranks.dev@gmail.com>
2022-05-02 19:02:14 -0400

    created the form UI and added real time form validation

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import FeedbackData from './data/FeedbackData'
 //components
 import Header from './components/Header'
 import FeedbackList from './components/FeedbackList'
+import FeedbackStats from './components/FeedbackStats'
+import FeedbackForm from './components/FeedbackForm'
 
 function App() {
   const [feedback, setFeeback] = useState(FeedbackData)
@@ -18,6 +20,8 @@ function App() {
     <>
       <Header />
       <div className='container'>
+        <FeedbackForm />
+        <FeedbackStats feedback={feedback} />
         <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
       </div>
     </>
