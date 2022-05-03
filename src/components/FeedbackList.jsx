86ea6accf9172067b0a7fd7cdbc3a6a70f63be0d
Brandon Franks <bgfranks.dev@gmail.com>
2022-05-02 20:32:24 -0400

    added framer motion to animate adding and deleting a review

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,4 +1,6 @@
 import PropTypes from 'prop-types'
+import { motion, AnimatePresence } from 'framer-motion'
+
 import FeedbackItem from './FeedbackItem'
 
 function FeedbackList({ feedback, handleDelete }) {
@@ -8,16 +10,38 @@ function FeedbackList({ feedback, handleDelete }) {
 
   return (
     <div className='feedback-list'>
-      {feedback.map((item) => (
-        <FeedbackItem
-          key={item.id}
-          rating={item.rating}
-          text={item.text}
-          handleDelete={handleDelete}
-          id={item.id}
-        />
-      ))}
+      <AnimatePresence>
+        {feedback.map((item) => (
+          <motion.div
+            key={item.id}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+          >
+            <FeedbackItem
+              key={item.id}
+              rating={item.rating}
+              text={item.text}
+              handleDelete={handleDelete}
+              id={item.id}
+            />
+          </motion.div>
+        ))}
+      </AnimatePresence>
     </div>
+
+    // return (
+    //   <div className='feedback-list'>
+    //     {feedback.map((item) => (
+    //       <FeedbackItem
+    //         key={item.id}
+    //         rating={item.rating}
+    //         text={item.text}
+    //         handleDelete={handleDelete}
+    //         id={item.id}
+    //       />
+    //     ))}
+    //   </div>
   )
 }
 
